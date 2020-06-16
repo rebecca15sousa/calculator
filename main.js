8d541f5e8ee9212edd278a1d8654882204a0834a
Rebecca Sousa <rebecca_sousa@hotmail.com>
2020-06-16 16:58:01 -0400

    Created function operate

--- main.js
+++ main.js
@@ -13,3 +13,28 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+function operate(operator, num1, num2) {
+    let res;
+
+    switch(operator) {
+        case "Add":
+            res = add(num1, num2);
+            console.log(res);
+            break;
+        case "Subtract":
+            res = subtract(num1, num2);
+            console.log(res);
+            break;
+        case "Multiply":
+            res = multiply(num1, num2);
+            console.log(res);
+            break;
+        case "Divide":
+            res = divide(num1, num2);
+            console.log(res);
+            break;
+        default:
+            return "ERROR"
+    }
+}
