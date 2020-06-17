6e2b436579e4ce22d0a684bbd2097e9f6aa71bb8
Rebecca Sousa <rebecca_sousa@hotmail.com>
2020-06-17 16:31:04 -0400

    Created functions populateDisplay and clearDisplay and event listeners for number buttons and clear button.

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+const display = document.querySelector('.display');
+const numButtons = document.querySelectorAll('.Nbtn');
+const clearButton = document.querySelector('.Cbtn');
+let numContent;
+let displayContent;
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -38,3 +44,27 @@ function operate(operator, num1, num2) {
             return "ERROR"
     }
 }
+
+function populateDisplay() {
+    displayContent = display.textContent;
+    if(displayContent === "0") {
+        display.textContent = numContent;
+    } else {
+        display.textContent = displayContent.concat(numContent);
+    }
+}
+
+function clearDisplay() {
+    display.textContent = "0";
+}
+
+clearButton.addEventListener('click', function() {
+    clearDisplay();
+});
+
+numButtons.forEach(function(Nbtn) {
+    Nbtn.addEventListener('click', function() {
+        numContent = Nbtn.textContent;
+        populateDisplay();
+    });
+});
