04174108bac363c3d66779a162e5d62271a8ef86
Rebecca Sousa <rebecca_sousa@hotmail.com>
2020-06-18 15:51:52 -0400

    Stored values for first and second numbers and operator, added event listener to equals button that gets these values and calls operate function.

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const numButtons = document.querySelectorAll('.Nbtn');
 const clearButton = document.querySelector('.Cbtn');
 const operButtons = document.querySelectorAll('.Obtn');
 const equalsButton = document.querySelector('.Ebtn');
+let res;
 let numContent;
 let displayContent;
 let previousKey;
@@ -27,23 +28,29 @@ function divide(num1, num2) {
 }
 
 function operate(operator, num1, num2) {
-    let res;
-
     switch(operator) {
         case "Add":
             res = add(num1, num2);
+            //displayContent = res;
+            display.textContent = res;
             console.log(res);
             break;
         case "Subtract":
             res = subtract(num1, num2);
+            //displayContent = res;
+            display.textContent = res;
             console.log(res);
             break;
         case "Multiply":
             res = multiply(num1, num2);
+            //displayContent = res;
+            display.textContent = res;
             console.log(res);
             break;
         case "Divide":
             res = divide(num1, num2);
+            //displayContent = res;
+            display.textContent = res;
             console.log(res);
             break;
         default:
@@ -55,9 +62,11 @@ function populateDisplay() {
     displayContent = display.textContent;
     if(displayContent === "0" || previousKey === "operator") {
         display.textContent = numContent;
+        //displayContent = display.textContent;
         previousKey = "";
     } else {
         display.textContent = displayContent.concat(numContent);
+        //displayContent = display.textContent;
     }
 }
 
@@ -79,6 +88,16 @@ numButtons.forEach(function(Nbtn) {
 operButtons.forEach(function(Obtn) {
     Obtn.addEventListener('click', function() {
         operatorValue = Obtn.dataset.action;
+        displayContent = display.textContent;
+        firstValue = displayContent;
         previousKey = "operator";
     });
 });
+
+equalsButton.addEventListener('click', function() {
+    displayContent = display.textContent;
+    secondValue = displayContent;
+    let firstValueNum = Number(firstValue);
+    let secondValueNum = Number(secondValue);
+    operate(operatorValue, firstValueNum, secondValueNum);
+})
