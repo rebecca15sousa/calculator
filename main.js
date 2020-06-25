1449854dc2d69b71b00daef8e9b0b892fb0dba4e
Rebecca Sousa <rebecca_sousa@hotmail.com>
2020-06-25 15:56:05 -0400

    Created backspace button

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const clearButton = document.querySelector('.Cbtn');
 const operButtons = document.querySelectorAll('.Obtn');
 const equalsButton = document.querySelector('.Ebtn');
 const decButton = document.querySelector('.Dbtn');
+const backspaceButton = document.querySelector('.Bbtn');
 let res;
 let numContent;
 let displayContent = "";
@@ -35,27 +36,27 @@ function operate(operator, num1, num2) {
         case "Add":
             res = add(num1, num2).toFixed(2);
             display.textContent = res;
-            console.log(res);
+            //console.log(res);
             break;
         case "Subtract":
             res = subtract(num1, num2).toFixed(2);
             display.textContent = res;
-            console.log(res);
+            //console.log(res);
             break;
         case "Multiply":
             res = multiply(num1, num2).toFixed(2);
             display.textContent = res;
-            console.log(res);
+            //console.log(res);
             break;
         case "Divide":
             if(num2 == 0) {
                 res = "Very funny...Try another number";
                 display.textContent = res;
-                console.log(res);
+                //console.log(res);
             } else {
                 res = divide(num1, num2).toFixed(2);
                 display.textContent = res;
-                console.log(res);
+                //console.log(res);
             }
             break;
         default:
@@ -71,7 +72,7 @@ function addNumber() {
         display.textContent = numContent;
         previousKey = "";
         consecutiveOperators = true;
-    } else if (previousKey === "equals") {
+    } else if(previousKey === "equals") {
         display.textContent = numContent;
         previousKey = "";
     } else {
@@ -93,6 +94,18 @@ function addDecimal() {
     }
 }
 
+function eraseCharacter() {
+    displayContent = display.textContent;
+    //console.log(displayContent);
+    let array = displayContent.split("");
+    //console.log(a);
+    let popped = array.pop();
+    //console.log(b);
+    let remain = array.join("");
+    //console.log(c);
+    display.textContent = remain;
+}
+
 function clearDisplay() {
     display.textContent = "0";
     firstValue = "";
@@ -102,6 +115,10 @@ function clearDisplay() {
     consecutiveOperators = false;
     consecutiveEquals = false;
 }
+
+backspaceButton.addEventListener('click', function() {
+    eraseCharacter();
+});
 
 clearButton.addEventListener('click', function() {
     clearDisplay();
